523fc72371e3892af48292d7b9e756262255afc3
urchgene <ugo3@cornell.edu>
2021-05-11 07:38:49 -0500

    Added wted GRMs

--- src/makeGRM.jl
+++ src/makeGRM.jl
@@ -18,6 +18,19 @@ function GRM(M)
 end
 
 
+function GRMwted(M, D)
+
+	M = Matrix(M);
+	#M = 1.0 .+ Matrix(M); # change to 0,1,2
+	P = mean(M, dims=1) ./2 ## freq
+	varsum = 2 * sum(P .* (1.0 .- P))
+	A = (M .- (2 .* P)) ;
+	K = (A * D * A') .* (1/varsum);
+	K = 0.99*K + 0.01*I
+	return(K)
+
+end
+
 function GRMinv(M)
 
         M = Matrix(M);
@@ -33,3 +46,17 @@ function GRMinv(M)
 
 end
 
+function GRMwtedinv(M, D)
+
+        M = Matrix(M);
+        #M = 1.0 .+ Matrix(M);
+        P = mean(M, dims=1) ./2
+        varsum = 2 * sum(P .* (1.0 .- P))
+        A = (M .- (2 .* P));
+        K = (A * D * A') .* (1/varsum);
+        K = 0.99*K + 0.01*I
+	K = cholesky(Positive, K);
+	K = inv(K);
+        return(K)
+
+end
