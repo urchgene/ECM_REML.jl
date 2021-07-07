75909358295d98a22d2929cfc238308fdffc1090
urchgene <ugo3@cornell.edu>
2021-07-07 15:07:46 -0500

    exported emmreml_LU function

--- src/EMMREML.jl
+++ src/EMMREML.jl
@@ -9,9 +9,11 @@ include("emmremlJulia.jl")
 include("emmremlMultivariate_Varcomp.jl")
 include("makeGRM.jl")
 include("makeRKHS.jl")
+include("emmreml_v2.jl")
 
 export emmreml, emmremlMultivariate
 export GRM, GRMinv, RKHS, RKHSinv, SqEuclid
 export GRMwted, GRMwtedinv, GRMiter
+export emmreml_LU
 
 end # module
